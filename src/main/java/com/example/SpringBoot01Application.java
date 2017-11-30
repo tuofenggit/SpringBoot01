17b4b5223793384a2941bd7352d72547da01434f
wangchuang <wangchuang@goldwind.com.cn>
2017-11-30 17:18:42 +0800

    测试push111

--- src/main/java/com/example/SpringBoot01Application.java
+++ src/main/java/com/example/SpringBoot01Application.java
@@ -23,6 +23,6 @@ public class SpringBoot01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot01Application.class, args);
 	}
-	// test push 11
+	// test push 11231321
 	 
 }
